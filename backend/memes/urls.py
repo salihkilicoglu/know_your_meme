0023f55a667a846f798ba988a2738c25b2aa7d6b
salihkilicoglu <salihkilicoglu@gmail.com>
2022-05-22 17:35:03 +0300

    api/memes/ & api/memes/create & api/memes/{pk} urls

--- backend/memes/urls.py
+++ backend/memes/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.api_home), # api/memes/
+    path('', views.ArticleListView.as_view(), name='article-list'), # api/memes/
+    path('create/', views.ArticleCreateView.as_view(), name='article-create'),
+    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
 ]
